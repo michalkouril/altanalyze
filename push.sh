725a00097a69d7424c3f027680f76a73cd8ec77c
Travis CI <travis@travis-ci.org>
2018-05-05 13:21:43 -0400

    Verbose push

--- push.sh
+++ push.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 setup_git() {
+  echo "Setup GIT"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_website_files() {
+  echo "Commit files"
   cd dist
-  tar -cvzf AltAnalyze.app.tar.gz AltAnalyze.app
+  tar -cvjf AltAnalyze.app.tar.bz2 AltAnalyze.app
+  cd ..
   git checkout -b osxapp
-  git add -f dist/AltAnalyze.app.tar.gz
+  git add -f dist/AltAnalyze.app.tar.bz2
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  echo "Upload files"
   git remote add origin-pages https://${GH_TOKEN}@github.com/michalkouril/altanalyze.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages osxapp 
 }
